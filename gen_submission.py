52b3c1e6bf92af61891bda23e922b68a8c583c9f
quannv-1204 <nguyen.van.quan@sun-asterisk.com>
2023-10-23 00:38:32 -0700

    add data & disease name

--- gen_submission.py
+++ gen_submission.py
@@ -1,11 +1,6 @@
-import time
-# from langchain.embeddings import HuggingFaceEmbeddings
 from embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma, FAISS
-from uuid import uuid4
-import gradio as gr
 from utils import jload
-from transformers import AutoTokenizer
 import csv
 from prompt_const import *
 import re
@@ -13,9 +8,7 @@ from llama_cpp_cuda import Llama
 from typing import List, Tuple, Dict, Any
 from utils import *
 
-# model = AutoModelForCausalLM.from_pretrained("pretrain/", model_file="fashiongpt-70b-v1.1.Q4_K_M.gguf", gpu_layers=75, context_length=8192)
 model = Llama(model_path="/mnt/sdd/nguyen.van.quan/Researchs/Qlora/pretrain/fashiongpt-70b-v1.1.Q4_K_M.gguf", n_ctx=8192, n_gqa=8, n_gpu_layers=100, rms_norm_eps=1e-5)
-tokenizer = AutoTokenizer.from_pretrained("VietnamAIHub/Vietnamese_LLama2_13B_8K_SFT_General_Domain_Knowledge", use_fast=False)
 
 embeddings_model_name  = "vinai/vinai-translate-vi2en"
 embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name, device="cuda")
@@ -35,7 +28,7 @@ def bot(query: str, options: str, test_case: Dict[str, str], max_new_tokens: int
     
     context = []
     for key in keywords:
-        inf = db_context.similarity_search_with_relevance_scores(key, k=3, score_threshold=context_score)
+        inf = db_context.similarity_search_with_relevance_scores(key, k=5, score_threshold=context_score)
         if len(inf) != 0:
             context.extend([i[0].metadata['content'] for i in inf])
 
@@ -60,8 +53,8 @@ def bot(query: str, options: str, test_case: Dict[str, str], max_new_tokens: int
     count = 0
     choices = ["0"]
     options_list = extract_options(options)
-    while is_invalid_format(choices) or not check_element(options_list, choices):
-        if count > 20:
+    while is_invalid_format(choices) or (not check_element(options_list, choices)):
+        if count > 10:
             text = "A, B"
             break
         generation_output = model(prompt, max_tokens=max_new_tokens, stream=False, temperature=0.4)
@@ -70,6 +63,7 @@ def bot(query: str, options: str, test_case: Dict[str, str], max_new_tokens: int
         count += 1
         choices = text.split(".")[0].split(", ")
         print(choices)
+    print(is_invalid_format(choices), (not check_element(options_list, choices)))
     output = binary_output(choices, test_case)  
     print(output)
     return output
@@ -78,16 +72,19 @@ def bot(query: str, options: str, test_case: Dict[str, str], max_new_tokens: int
 if __name__ == "__main__":
     test_datas = jload("data/ChallengeTestData.json")
 
-    with open('result.csv', 'w', encoding='UTF8') as f:
+    with open('result.csv', 'a', encoding='UTF8') as f:
         writer = csv.writer(f)
         header = ['id', 'answer']
         # write the header
-        writer.writerow(header)
+        # writer.writerow(header)
 
-        for data in test_datas:
+        for data in test_datas[27:]:
             question = data["question"]
             options = data["options"]
             options = fix_options_format(options)
             output = bot(question, options, data)
             writer.writerow([data['id'], output])
-        
+
+# regen query
+# fintune aquila 34b
+# 
